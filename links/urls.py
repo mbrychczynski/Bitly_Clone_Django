088b3215a965fab84a16d5aa10eab355478d8188
Mateusz Brychczynski <mat.brych@gmail.com>
2024-10-24 14:42:11 +0100

    Adding Link Shortening Redirect Functionality

--- links/urls.py
+++ links/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import index
+from .views import index, root_link
 
 urlpatterns = [
     path("", index, name="home"),
+    path("<str:link_slug>/", root_link, name="root-link"),
 ]
--- links/views.py
+++ links/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Link
 
 # Create your views here.
@@ -6,3 +6,9 @@ def index(request):
     links = Link.objects.all()
     context = {'links': links}
     return render(request, 'links/index.html', context)
+
+def root_link(request, link_slug):
+    link = get_object_or_404(Link, slug=link_slug)
+    link.click()
+
+    return redirect(link.url)
